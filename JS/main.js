34ba553dca4c173c348c21a088dbd536c59e9b5e
Sanchelloz <alekseichuk.a@gmail.com>
2017-04-26 20:32:11 +0300

    Добавлено построение таблицы со стилями

--- JS/main.js
+++ JS/main.js
@@ -2,13 +2,16 @@ function TestLoadData() {
     $.ajax("http://www.json-generator.com/api/json/get/bUsRkvEmHm?indent=2",
         { type:'GET', dataType:'json', success:DataLoaded, error:ErrorHandler }
     );
-
 };
+
 TestLoadData();
 
+var Base = [];
+
 function DataLoaded(data) {
-    //base = data;
+    Base = data;
     console.log(data)
+    return data;
 };
 
 
@@ -32,7 +35,16 @@ $('#clearSelection').on('click', function(){
 });
 
 $(".Submit_btn").on("click", function(){
-    console.log($('[checked="checked"]'))
+    var requestedData = $('[checked="checked"]');
+    console.log(requestedData);
+    var requestedId = [];
+    for (var i=0; i<requestedData.length; i++ ){
+        
+        var id = requestedData[i].id;
+        requestedId.push(id);
+    }
+    //console.log(requestedId);
+    BuildTable(requestedId)
 })
 
 $(window).on('load', function () {
@@ -42,8 +54,45 @@ $(window).on('load', function () {
     $preloader.delay(350).fadeOut('slow');
     $("#page_data_selection").delay(1000).show(0);
 });
+function BuildTable(idArr){
+    $('#requested_data thead tr').html("");
+    $('#requested_data tbody').html("");
+    var count = 0;
+    Start:
+    for (var k=0; k<Base.length; k++){
+            
+        $('#requested_data tbody').append("<tr></tr>");
+        
+        for (var j=0; j<idArr.length; j++){
+
+            var b = idArr[j];
+            if (b=="id")    {
+                b="_id";
+            }
+            
+            
+            $('#requested_data tbody tr:eq('+k+')').append("<td>" + Base[k][b] + "</td>")
+
+        }
+        if (count != 0){
+            continue Start;
+        }
+        for (var m=0; m<idArr.length; m++){
+
+            var c = idArr[m];
+            if (c=="id")    {
+                c="#id";
+            }
+
+            $('#requested_data thead tr').append("<th>" + c + "</th>");
+        }
+        count = 1;
+    }
+    
+    
+}
 
 
 function ErrorHandler(jqXHR,StatusStr,ErrorStr){
-    alert(StatusStr+' '+ErrorStr);
+    alert(StatusStr+'Загрузка не удалась'+ErrorStr);
 }
